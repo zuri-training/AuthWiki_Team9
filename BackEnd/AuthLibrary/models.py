51d3f0efc2d239f1253c906c12ca211bd8798f28
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-07-26 06:50:43 +0100

    Backend Project Setup #1

---
+++ BackEnd/AuthLibrary/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+
+class AuthLibrary(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    # comment = models
+    # code_snippets = models
+    no_of_downloads = models.PositiveIntegerField()
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
