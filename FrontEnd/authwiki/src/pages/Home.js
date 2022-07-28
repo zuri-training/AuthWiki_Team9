b141cdd1143c7e97e80871991f539e916a87fc0b
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-07-28 10:21:24 +0100

    Implement the getting started Section  #33

--- FrontEnd/authwiki/src/pages/Home.js
+++ FrontEnd/authwiki/src/pages/Home.js
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+import GettingStarted from "../components/GettingStarted";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <GettingStarted />
+    </React.Fragment>
+  );
 };
 
 const Wrapper = styled.h1`
