1a706dfa590aa9ca2721298202b3442df5a76b0d
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-07-28 08:29:28 +0100

    Implemented reaction model in the authlibrary #47

--- BackEnd/AuthLibrary/admin.py
+++ BackEnd/AuthLibrary/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import AuthLibrary, Reactions
+from .models import AuthLibrary, Reaction
 
 # Register your models here.
 
 admin.site.register(AuthLibrary)
-admin.site.register(Reactions)
+admin.site.register(Reaction)
