5ccf130e8d13dd149dfd2bd0929e65456c9cbb2d
CodeWithBilal <bilalmubarakidris@gmail.com.com>
2022-07-29 12:28:30 +0530

    Implement Hero Section (UI) #14 and Implement a List of all Authentications Libraries (UI) #18

--- FrontEnd/authwiki/src/components/HeroSection.js
+++ FrontEnd/authwiki/src/components/HeroSection.js
@@ -1,7 +1,20 @@
 import React from "react";
+import '../css/HeroSection.css'
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return(
+    <div className="hero">
+        <div className="title-wrapper">
+            <h2 className="title copy">Copy.</h2>
+            <h2 className="title paste">Paste.</h2>
+            <h2 className="title test">Test.</h2>
+        </div>
+        <a href="#" className="docs-btn">Documentation</a>
+            <div className='para-container'>
+                <p className='desc'><a href='#'>Authlt</a> combines the best developer experience with an obsessive focus on end-user performance. <span>Our paltform enables frontend teams to do their best work.</span></p>
+            </div>
+    </div>
+)
 };
 
 export default HeroSection;
