73bfd0cf322a2d24a7c426e57d726e946c0df200
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-07-29 08:15:18 +0100

    Implement Serializers, Views and URLs of Auth Library  #64

--- BackEnd/AuthLibrary/admin.py
+++ BackEnd/AuthLibrary/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AuthLibrary, Reaction
+from .models import AuthLibrary, Reaction, CodeSnippet
 
 # Register your models here.
 
+admin.site.register(CodeSnippet)
 admin.site.register(AuthLibrary)
 admin.site.register(Reaction)
--- BackEnd/AuthLibrary/models.py
+++ BackEnd/AuthLibrary/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth import get_user_model
 
 
 # Create your models here.
+class CodeSnippet(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} || {str(self.id)}"
+
 
 class AuthLibrary(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     # comment = models
-    # code_snippets = models
+    # code_snippets = models.OneToOneField(CodeSnippet, on_delete=models.CASCADE, default=None)
     no_of_downloads = models.PositiveIntegerField()
     created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -19,12 +28,9 @@ class AuthLibrary(models.Model):
 
 
 class Reaction(models.Model):
-    
     upvote = models.IntegerField(default=0)
     down_vote = models.IntegerField(default=0)
     no_of_vote = models.PositiveIntegerField()
 
     def __str__(self):
         return f"{str(self.upvote)}"
-
-
