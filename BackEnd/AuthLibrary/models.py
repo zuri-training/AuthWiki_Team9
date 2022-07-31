03f98be96577c8d8f8464aa3ef8f1bb9e7f9c3ca
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-07-31 15:28:19 +0100

    Database Schema: Mapping of Relationships #75

--- BackEnd/AuthLibrary/models.py
+++ BackEnd/AuthLibrary/models.py
@@ -8,20 +8,20 @@ class CodeSnippet(models.Model):
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    auth_library = models.ForeignKey('CodeSnippet',
+                                     on_delete=models.CASCADE,
+                                     related_name='code_snippet')
 
     def __str__(self):
         return f"{self.title} || {str(self.id)}"
 
 
-
-
 class AuthLibrary(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    # comment = models
-    # code_snippets = models.OneToOneField(CodeSnippet, on_delete=models.CASCADE, default=None)
     no_of_downloads = models.PositiveIntegerField()
-    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_by = models.ForeignKey(get_user_model(),
+                                   on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -32,24 +32,26 @@ class AuthLibrary(models.Model):
 class Reaction(models.Model):
     upvote = models.IntegerField(default=0)
     down_vote = models.IntegerField(default=0)
-    no_of_vote = models.PositiveIntegerField()
 
     def __str__(self):
         return f"{str(self.upvote)}"
 
 
 class Comment(models.Model):
-    author_Name = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    comment_body = models.TextField( blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    author_name = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    comment_body = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    auth_library = models.ForeignKey(AuthLibrary, on_delete=models.CASCADE,
+                                     related_name='comment')
+    reaction = models.OneToOneField(Reaction, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{str(self.author_Name)}"
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255, null=True)
-    slug = models.SlugField(null=True)
-    description = models.CharField(max_length=500, null=True,blank=True, verbose_name='Description')
+    description = models.CharField(max_length=255, null=True, blank=True, verbose_name='Description')
 
     def __str__(self):
         return str(self.name)
