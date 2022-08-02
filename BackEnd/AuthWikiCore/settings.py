45ddba206944921dc2f47dba8d829eada84a6dcb
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-08-02 09:23:00 +0100

    BUG: fix the upload location #82

--- BackEnd/AuthWikiCore/settings.py
+++ BackEnd/AuthWikiCore/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/static/'
 MEDIA_URL = 'static/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
