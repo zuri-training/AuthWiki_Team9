569e99889c09656da02452cfc9de4df28cf03e2e
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-08-02 17:34:58 +0100

    Code Refactor for the Backend, Delete the reaction table #105

--- BackEnd/AuthLibrary/views.py
+++ BackEnd/AuthLibrary/views.py
@@ -43,26 +43,22 @@ class CommentAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         pk = self.kwargs['pk']
-        comment_body = self.request.data.get('comment_body')
         auth_lib = AuthLibrary.objects.get(pk=pk)
         user = self.request.user
-        serializer.comment_body = comment_body
-        serializer.save(author_name=user, auth_library=auth_lib)
+        serializer.save(**self.request.data,
+                        author_name=user, auth_library=auth_lib)
 
 
 class CodeSnippetAPIView(generics.CreateAPIView):
     serializer_class = CodeSnippetSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = []
+    permission_classes = [IsAdminUser]
 
     def get_object(self):
         return CodeSnippet.objects.all()
 
     def perform_create(self, serializer):
         pk = self.kwargs['pk']
-        title = self.request.data.get('title')
-        description = self.request.data.get('description')
         auth_lib = AuthLibrary.objects.get(pk=pk)
-        serializer.title = title
-        serializer.description = description
-        serializer.save(auth_library=auth_lib)
+        serializer.save(**self.request.data,
+                        auth_library=auth_lib)
