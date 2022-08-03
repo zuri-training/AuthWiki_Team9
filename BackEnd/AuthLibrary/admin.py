607eb5e0e56ce7a2da5a0240199692e578967601
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-08-03 19:46:25 +0100

    Remove for the Category Database Model #82

--- BackEnd/AuthLibrary/admin.py
+++ BackEnd/AuthLibrary/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AuthLibrary, Category, Comment, CodeSnippet, DownVote, UpVote
+from .models import AuthLibrary, Comment, CodeSnippet, DownVote, UpVote
 
 
 # Register your models here.
@@ -8,7 +8,6 @@ from .models import AuthLibrary, Category, Comment, CodeSnippet, DownVote, UpVot
 admin.site.register(CodeSnippet)
 admin.site.register(AuthLibrary)
 admin.site.register(Comment)
-admin.site.register(Category)
 admin.site.register(DownVote)
 admin.site.register(UpVote)
 
