3d2be7ba1435ab7520428f0e75604ccf8d46ddff
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-08-06 16:56:04 +0100

    Syntax Highlighting with Prismjs #131

---
+++ FrontEnd/authwiki/src/components/Code.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "./styles.css";
+
+export default function Code({ code, language }) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  return (
+    <div className="Code">
+      <pre>
+        <code className={`language-${language}`}>{code}</code>
+      </pre>
+    </div>
+  );
+}
