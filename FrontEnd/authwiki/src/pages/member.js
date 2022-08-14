fd19d1d55ccbb1d16f05e1b8072ea85b35265a1e
Christian Ajibodu <70984807+Christian-03@users.noreply.github.com>
2022-08-14 15:15:18 +0100

    Members Update

--- FrontEnd/authwiki/src/pages/member.js
+++ FrontEnd/authwiki/src/pages/member.js
@@ -20,19 +20,19 @@ const Members = [
     title: "Lead Developer",
   },
   {
-    image: "https://i.ibb.co/bBkTs3D/Passport-Adeyemo-Ridwan-Damilare.jpg",
-    name: "Adeyemo Ridwan",
-    title: "Lead Developer",
+    image: "<a href="https://ibb.co/CMJtV8B"><img src="https://i.ibb.co/3MfsSd1/85946139-E2-E9-4-B37-8-A24-52724-BEA964-F.jpg" alt="85946139-E2-E9-4-B37-8-A24-52724-BEA964-F" border="0"></a>",
+    name: "Evangel Soribe",
+    title: "Full-Stack Developer",
   },
   {
-    image: "https://i.ibb.co/bBkTs3D/Passport-Adeyemo-Ridwan-Damilare.jpg",
-    name: "Adeyemo Ridwan",
-    title: "Lead Developer",
+    image: "<a href="https://ibb.co/fdgqqVr"><img src="https://i.ibb.co/B2pccHT/IMG-5020.jpg" alt="IMG-5020" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>free high resolution photos</a><br />",
+    name: "Oribi Tiffany Poloamina",
+    title: "Product Designer",
   },
   {
-    image: "https://i.ibb.co/bBkTs3D/Passport-Adeyemo-Ridwan-Damilare.jpg",
-    name: "Adeyemo Ridwan",
-    title: "Lead Developer",
+    image: "<a href="https://ibb.co/R4WvHRM"><img src="https://i.ibb.co/k6FHcNr/2-FAC8-D6-B-062-D-49-BA-8669-8834-DA14-C4-ED.jpg" alt="2-FAC8-D6-B-062-D-49-BA-8669-8834-DA14-C4-ED" border="0"></a>",
+    name: "Fehintola Bolagbade",
+    title: "Product Designer",
   },
   {
     image: "https://i.ibb.co/bBkTs3D/Passport-Adeyemo-Ridwan-Damilare.jpg",
