f1ce66307c4c501cab1aaed77e33846bf07dcbb6
Ridwan Adeyemo <adesolaridwan2003@gmail.com>
2022-08-15 12:15:26 +0100

    update for Oyeyode Fikayo

--- FrontEnd/authwiki/src/pages/member.js
+++ FrontEnd/authwiki/src/pages/member.js
@@ -51,9 +51,9 @@ const Members = [
     title: "Full-Stack Developer",
   },
   {
-    image: "https://i.ibb.co/bBkTs3D/Passport-Adeyemo-Ridwan-Damilare.jpg",
-    name: "Adeyemo Ridwan",
-    title: "Lead Developer",
+    image: "https://i.ibb.co/vVWfrhk/IMG-2876.jpg",
+    name: "Oyeyode Fikayo",
+    title: "Frontend Developer",
   },
 ];
 
