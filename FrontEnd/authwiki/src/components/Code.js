8ba1520a1b897dd89eb06cefcd6eff0b9a8d021f
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-08-16 17:59:12 +0100

    add code Clipboard

--- FrontEnd/authwiki/src/components/Code.js
+++ FrontEnd/authwiki/src/components/Code.js
@@ -1,16 +1,41 @@
 import React, { useEffect } from "react";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
+import { MdOutlineContentCopy } from "react-icons/md";
+import styled from "styled-components";
+import { toast } from "react-toastify";
 
 export default function Code({ code, language }) {
   useEffect(() => {
     Prism.highlightAll();
   }, []);
   return (
-    <div className="Code">
-      <pre>
-        <code className={`language-${language}`}>{code}</code>
-      </pre>
-    </div>
+    <CSS>
+      <div className="Code">
+        <pre>
+          <MdOutlineContentCopy
+            size="20px"
+            className="copy"
+            onClick={() => {
+              navigator.clipboard.writeText(code);
+              toast.success("Copied To Clipboard");
+              return;
+            }}
+          />
+          <code className={`language-${language}`}>{code}</code>
+        </pre>
+      </div>
+    </CSS>
   );
 }
+
+const CSS = styled.div`
+  .Code {
+    position: relative;
+  }
+  .copy {
+    position: absolute;
+    right: 20px;
+    cursor: pointer;
+  }
+`;
